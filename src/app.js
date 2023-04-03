2e5b88d435b4de5f7cbd2cfd5fb6e2cbdd5a23e4
Maxwell <Maxwellbarboza@hotmail.com>
2023-04-03 13:58:14 -0300

    Foi criado o arquivo app

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+const livros = [
+    {id: 1, "titulo": "Senhor dos Aneis"},
+    {id: 2, "titulo": "O Hobiit"}
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('Curso de Node');
+})
+
+app.get('/livros', (req,res) => {
+    res.status(200).send("Livros")
+})
+
+export default app;
+
