598f319f434d992b145712170b8cd57ac828e99c
Maxwell <Maxwellbarboza@hotmail.com>
2023-04-06 12:12:50 -0300

    Foi incluido os tratamentos de Erro middlewares.

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,62 +1,107 @@
+import NaoEncontrado from "../erros/NaoEncontrado.js";
 import livros from "../models/Livro.js";
 
 class LivroController {
 
-    static listarLivros = (req, res) => {
-      livros.find((err, livros) => {
-        res.status(200).json(livros)
-    })
-    }
+  static listarLivros = async (req, res, next) => {
+    try{
+      
+      const livrosResultado = await livros.find()
+        .populate("autor")
+        .exec();             
+      res.status(200).json(livrosResultado);  
+
+    }catch(erro){
+
+      next(erro);
+    }    
+  };
   
-    static listarLivroPorId = (req, res) => {
-      const id = req.params.id;
+  static listarLivroPorId = async (req, res, next) => {
+    
+    try{
+      const id = req.params.id;  
+      const livroResultado = await livros.findById(id)
+        .populate("autor", "nome" )
+        .exec();  
+       
+      if(livroResultado !== null) {
+        res.status(200).send(livroResultado);
+            
+      } else {
+
+        next(new NaoEncontrado("Id do livro não localizado."));            
+      }      
+
+    }catch(erro){
+
+      next(erro);
+    }   
+  };
   
-      livros.findById(id, (err, livros) => {
-        if(err) {
-          res.status(400).send({message: `${err.message} - Id do livro não localizado.`})
-        } else {
-          res.status(200).send(livros);
-        }
-      })
-    }
+  static cadastrarLivro = async (req, res, next) => {
+    try{
+
+      let livro = new livros(req.body);  
+      const livroResultado = await livro.save();
   
-    static cadastrarLivro = (req, res) => {
-      let livro = new livros(req.body);
+      res.status(201).send(livroResultado.toJSON());   
+
+    }catch(erro){
+
+      next(erro);
+    }   
+  };
   
-      livro.save((err) => {
+  static atualizarLivro = async (req, res, next) => {
+    try{
+      const id = req.params.id;  
+      const livroResultado = await livros.findByIdAndUpdate(id, {$set: req.body});
+      
+      if(!livroResultado !== null) {
+        res.status(200).send({message: "Livro atualizado com sucesso"});
+      } else {
+        next(new NaoEncontrado("Id do livro não localizado."));
+      }      
+
+    }catch(erro){
+
+      next(erro);
+    }    
+  };
   
-        if(err) {
-          res.status(500).send({message: `${err.message} - falha ao cadastrar livro.`})
-        } else {
-          res.status(201).send(livro.toJSON())
-        }
-      })
-    }
+  static excluirLivro = async (req, res, next) => {
+    try{
+      const id = req.params.id;  
+      const livrosResultado = await livros.findByIdAndDelete(id); 
+      if(livrosResultado !== null){
+        res.status(200).send({message: "Livro removido com sucesso"});
+      } else {
+        next(new NaoEncontrado("Livro não encontrado"));
+      }     
+
+    }catch(erro){
+
+      next(erro);
+    }    
+  };
+
+  static listarLivroPorEditora = async (req, res, next) => {
+    try{
+      const editora = req.query.editora;
+
+      const livrosResultado = await livros.find({"editora": editora}, {});
+      if(livrosResultado !== null){  
+        res.status(200).send(livros);
+      }else{
+        next(new NaoEncontrado("Livro não encontado."));
+      }
+
+    }catch(erro){
+
+      next(erro);
+    }    
+  };  
+}
   
-    static atualizarLivro = (req, res) => {
-      const id = req.params.id;
-  
-      livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
-        if(!err) {
-          res.status(200).send({message: 'Livro atualizado com sucesso'})
-        } else {
-          res.status(500).send({message: err.message})
-        }
-      })
-    }
-  
-    static excluirLivro = (req, res) => {
-      const id = req.params.id;
-  
-      livros.findByIdAndDelete(id, (err) => {
-        if(!err){
-          res.status(200).send({message: 'Livro removido com sucesso'})
-        } else {
-          res.status(500).send({message: err.message})
-        }
-      })
-    }
-  
-  }
-  
-  export default LivroController
+export default LivroController;
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema(
-    {
-        id:     {type: String},
-        titulo: {type: String, required: true},
-        autor:  {type: String, required: true },
-        editora: {type: String, required: true},
-        numeroPaginas: {type: Number}
-    }
+  {
+    id:     {type: String},
+    titulo: {
+      type: String, 
+      required: [true, "O título do livro é obrigatório."]
+    },
+    autor:  {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "autores", 
+      required: [true, "O nome do autor é obrigatório."] 
+    },
+    editora: {
+      type: String, 
+      required: [true, "O nome da editora é obrigatório."]
+    },
+    numeroPaginas: {type: Number}
+  }
 );
 
-const livros = mongoose.model('livros', livroSchema);
+const livros = mongoose.model("livros", livroSchema);
 export default livros;
